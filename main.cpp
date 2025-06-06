4faa07f19d5056f70a6732e12653080d043f165e
Luka Hauko <73116082+Luxha00@users.noreply.github.com>
2025-06-06 12:58:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -81,31 +81,17 @@ int main() {
 
     OQS_init();
 
-    // Print all available algorithms
-    // Print all available algorithms
-    std::cout << "Razpoložljivi algoritmi v liboqs (" << OQS_version() << "):\n";
-    for (size_t i = 0; i < OQS_KEM_alg_count(); i++) {
-        const char* alg_id = OQS_KEM_alg_identifier(i);
-        std::cout << "  [" << i << "] " << alg_id;
-        if (OQS_KEM_alg_is_enabled(alg_id)) {
-            std::cout << " (enabled)";
-        } else {
-            std::cout << " (disabled)";
-        }
-        std::cout << "\n";
-    }
-
     const int ITERATIONS = 1000;
     const char* KEM_ALGORITHMS[] = {
         // BIKE
-        //"BIKE-L1", "BIKE-L3", "BIKE-L5",
+        "BIKE-L1", "BIKE-L3", "BIKE-L5",
         
         // Classic McEliece
-        //"Classic-McEliece-348864", "Classic-McEliece-348864f",
-        //"Classic-McEliece-460896", "Classic-McEliece-460896f",
-        //"Classic-McEliece-6688128", "Classic-McEliece-6688128f",
-        //"Classic-McEliece-6960119", "Classic-McEliece-6960119f",
-        //"Classic-McEliece-8192128", "Classic-McEliece-8192128f",
+        "Classic-McEliece-348864", "Classic-McEliece-348864f",
+        "Classic-McEliece-460896", "Classic-McEliece-460896f",
+        "Classic-McEliece-6688128", "Classic-McEliece-6688128f",
+        "Classic-McEliece-6960119", "Classic-McEliece-6960119f",
+        "Classic-McEliece-8192128", "Classic-McEliece-8192128f",
         
         // HQC
         "HQC-128", "HQC-192", "HQC-256",
