312d268a2a664396496bddc19b151f6da970c578
Luka Hauko <73116082+Luxha00@users.noreply.github.com>
2025-10-04 13:22:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -87,11 +87,11 @@ int main() {
         "BIKE-L1", "BIKE-L3", "BIKE-L5",
         
         // Classic McEliece
-        "Classic-McEliece-348864", "Classic-McEliece-348864f",
-        "Classic-McEliece-460896", "Classic-McEliece-460896f",
-        "Classic-McEliece-6688128", "Classic-McEliece-6688128f",
-        "Classic-McEliece-6960119", "Classic-McEliece-6960119f",
-        "Classic-McEliece-8192128", "Classic-McEliece-8192128f",
+        //"Classic-McEliece-348864", "Classic-McEliece-348864f",
+        //"Classic-McEliece-460896", "Classic-McEliece-460896f",
+        //"Classic-McEliece-6688128", "Classic-McEliece-6688128f",
+        //"Classic-McEliece-6960119", "Classic-McEliece-6960119f",
+        //"Classic-McEliece-8192128", "Classic-McEliece-8192128f",
         
         // HQC
         "HQC-128", "HQC-192", "HQC-256",
